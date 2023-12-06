b260fe26ab8b2a81f46357dc7bb6755fa6194de0
Eecodev <mnasser.bbk@gmail.com>
2023-12-06 17:44:06 +0100

    git update

--- Views/header.php
+++ Views/header.php
@@ -5,6 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css" integrity="sha512-z3gLpd7yknf1YoNbCzqRKc4qyor8gaKU1qmn+CShxbuBusANI9QpRohGBreCFkKxLhei6S9CQXFEbbKuqLg0DA==" crossorigin="anonymous" referrerpolicy="no-referrer" />
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+    <link rel="stylesheet" href="css/style.css">
     <title>Document</title>
 </head>
 
@@ -12,13 +13,13 @@
 <body>
 
     <header>
-        <div class="container">
+        <div class="container d-flex justify-content-between align-items-center">
             <h1>OOP - Shop</h1>
             
-            <ul>
-                <li><a href="index.php">MOVIES</a></li>
-                <li><a href="Readings.php">BOOKS</a></li>
-                <li><a href="PcGames.php">PC GAMES</a></li>
+            <ul class="menu d-flex">
+                <li class="p-2 menu-item"><a href="index.php">MOVIES</a></li>
+                <li class="p-2 menu-item"><a href="Readings.php">BOOKS</a></li>
+                <li class="p-2 menu-item"><a href="PcGames.php">PC GAMES</a></li>
             </ul>
         </div>
     </header>
